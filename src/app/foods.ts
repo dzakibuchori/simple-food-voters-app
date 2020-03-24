b586182ac1a88fb90566ff062f6db01d138c5cef
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-24 17:42:17 +0700

    update foods.ts

--- src/app/foods.ts
+++ src/app/foods.ts
@@ -1,20 +1,26 @@
 export const foods = [
   {
+    id: 1,
     name: 'Buah-buahan segar',
     description: 'Terdiri dari apel, manggis, jeruk, dan semangka.',
-    img_url : 'buah.jpg',
+    img_name : 'buah',
+    img_id : 1,
     vote: 0,
   },
   {
+    id: 2,
     name: 'Unknown food',
     description: 'Bahan makanan tidak diketahui',
-    img_url : 'unknown.jpg',
+    img_name : 'unknown',
+    img_id : 1,
     vote: 0,
   },
   {
+    id: 3,
     name: 'Cake Merah Delima',
     description: 'Rasanya bikin lidah bergoyang sendirian',
-    img_url : 'cake.jpg',
+    img_name : 'cake',
+    img_id : 1,
     vote: 0,
   },
 ];
