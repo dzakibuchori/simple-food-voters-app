4033593baeccf7e582a54fb0d5fd3db95961c8aa
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-24 17:42:07 +0700

    update food-list component

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -15,4 +15,29 @@ export class FoodListComponent implements OnInit {
   ngOnInit() {
   }
 
+  removeFood(id: number){
+    const index = this.foods.findIndex(food => food.id === id);
+    this.foods.splice(index, 1);
+  }
+
+  upFood(id: number){
+    const index = this.foods.findIndex(food => food.id === id);
+    this.foods[index].vote += 1;
+  }
+
+  downFood(id: number){
+    const index = this.foods.findIndex(food => food.id === id);
+    this.foods[index].vote -= 1;
+  }
+
+  rightFootImgUrl(id: number, ){
+    const index = this.foods.findIndex(food => food.id === id);
+    (this.foods[index].img_id < 3) ? this.foods[index].img_id += 1 : alert('you reach the max!');
+  }
+
+  leftFootImgUrl(id: number){
+    const index = this.foods.findIndex(food => food.id === id);
+    (this.foods[index].img_id > 1) ? this.foods[index].img_id -= 1 : alert('you reach the minimum!');
+  }
+
 }
